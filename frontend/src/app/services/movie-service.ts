5d19154ba674cee031a6a98355c3f75e68acfb76
Martin Didolich <marindidolich2012@gmail.com>
2025-09-13 12:59:45 -0300

    FE environment setup

--- frontend/src/app/services/movie-service.ts
+++ frontend/src/app/services/movie-service.ts
@@ -1,20 +1,18 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Movie } from '../interfaces/Movie';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  private apiUri = 'https://api.themoviedb.org/3/';
-  private token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYTA2ZjliM2VhNjA4ZGYyYzI0ZDhhZDI0Mjk4ODE2MyIsIm5iZiI6MTc1NzcyMTUxNy4xMjk5OTk5LCJzdWIiOiI2OGM0YjNhZDkxYjI0MGNmYzA0NDJiOWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.UvMDHDX435jHmuyu_cszk6Nprde7HXSUrK0AhVDZOaE';
-
   private readonly http = inject(HttpClient);
 
   getMovies() {
-    const url = `${this.apiUri}movie/11`;
+    const url = `${environment.movieApiUrl}movie/11`;
     const headers: HttpHeaders = new HttpHeaders({
-      Authorization: `Bearer ${this.token}`,
+      Authorization: `Bearer ${environment.movieApiToken}`,
     });
 
     return this.http.get<Movie>(url, { headers });
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:3000',
+  movieApiUrl: 'https://api.themoviedb.org/3/',
+  movieApiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYTA2ZjliM2VhNjA4ZGYyYzI0ZDhhZDI0Mjk4ODE2MyIsIm5iZiI6MTc1NzcyMTUxNy4xMjk5OTk5LCJzdWIiOiI2OGM0YjNhZDkxYjI0MGNmYzA0NDJiOWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.UvMDHDX435jHmuyu_cszk6Nprde7HXSUrK0AhVDZOaE'
+};
