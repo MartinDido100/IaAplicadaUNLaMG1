7d94879f26050e23923d2a8ba5f5ad32d3f2895f
Juan Bianchi <juanbianchi@icloud.com>
2025-09-21 00:42:56 -0300

    feat: First approach

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,1 +1,2 @@
 export * from './Movie.js';
+export * from './User.js';
--- api/src/router/index.ts
+++ api/src/router/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { recommendationRouter } from '../controllers/RecommendationController.js';
+import { authRouter } from '../controllers/AuthController.js';
 
 export const router = Router();
 
 router.use('/health', recommendationRouter);
+router.use('/recommendations', recommendationRouter);
+router.use('/auth', authRouter);
